45bae8318f860cf719a1da15ff48e8c76302c388
Numberer <ybs1164@naver.com>
2023-09-27 02:02:09 +0000

    speed

--- src/gameScene.js
+++ src/gameScene.js
@@ -14,6 +14,8 @@ export class GameScene extends Phaser.Scene {
     create() {
         this.player = this.add.circle(200, 200, 20, 0x6666ff);
 
+        this.speed = 5;
+
         this.keyup = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
         this.keydown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         this.keyright = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
@@ -21,17 +23,23 @@ export class GameScene extends Phaser.Scene {
     }
 
     update(time) {
+        let keyVector = new Phaser.Math.Vector2(0);
+
         if (this.keyup.isDown) {
-            this.player.y += 1;
+            keyVector.y -= 1;
         }
         if (this.keydown.isDown) {
-            this.player.y -= 1;
+            keyVector.y += 1;
         }
         if (this.keyright.isDown) {
-            this.player.x += 1;
+            keyVector.x += 1;
         }
         if (this.keyleft.isDown) {
-            this.player.x -= 1;
+            keyVector.x -= 1;
         }
+        keyVector.normalize().scale(this.speed);
+
+        this.player.x += keyVector.x;
+        this.player.y += keyVector.y;
     }
 }
