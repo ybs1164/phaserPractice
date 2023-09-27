229b9f292d3bbb1770c463c82717b82cf6064a1b
Numberer <ybs1164@naver.com>
2023-09-27 01:43:35 +0000

    moving

--- src/gameScene.js
+++ src/gameScene.js
@@ -12,9 +12,26 @@ export class GameScene extends Phaser.Scene {
 
     }
     create() {
-        this.add.circle(100, 100, 10);
-    }
-    update(time) {
+        this.player = this.add.circle(200, 200, 20, 0x6666ff);
 
+        this.keyup = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        this.keydown = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        this.keyright = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
+        this.keyleft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+    }
+
+    update(time) {
+        if (this.keyup.isDown) {
+            this.player.y += 1;
+        }
+        if (this.keydown.isDown) {
+            this.player.y -= 1;
+        }
+        if (this.keyright.isDown) {
+            this.player.x += 1;
+        }
+        if (this.keyleft.isDown) {
+            this.player.x -= 1;
+        }
     }
 }
